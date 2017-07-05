ca672724abcc05c945b3e222782c817e4f8715d3
johnmlhll <johnmlhll@yahoo.com>
2017-07-05 15:24:36 +0100

    Commit No 1 - Fresh JDBC MySQL Implementation with Insert | Part Implementation of Search Function

---
+++ src/javaswinggui/projectpersonaldetails/JavaSwingGUIProjectPersonalDetails.java
@@ -0,0 +1,20 @@
+package javaswinggui.projectpersonaldetails;
+/**
+ * Program Definition: GUI Form in Java using Swing to create a simple personal
+ * details form. This application will use GitHub for version control.
+ * @author jmulhall@yahoo.com twitter: @johnmlhll
+ */
+public class JavaSwingGUIProjectPersonalDetails {
+
+    /**
+     * Class definition: main method to manage the program starting with the
+     * GUI form to input personal details. Future builds include extra form
+     * and MySQL DB for Personal and Contact(fk personalId) tables 
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //instantiate PersonalDetailsForm and call the constructor
+        new PersonalDetailsForm();
+    }
+    
+}
